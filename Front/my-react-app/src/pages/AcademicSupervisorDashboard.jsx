2bd11a36ccf8421d97cddbf35a5e858f7bc878c7
Nalule Mable Muwanguzi <nalulemablemuwanguzi@gmail.com>
2026-03-26 16:29:32 +0300

    Added 4 dashboard pages and updated App.jsx routes

---
+++ Front/my-react-app/src/pages/AcademicSupervisorDashboard.jsx
@@ -0,0 +1,4 @@
+function AcademicSupervisorDashboard() {
+return <div><h2>WorkplaceSupervisor Dashboard</h2><p>Welcome, Academic Supervisor.</p></div>;
+}
+export default AcademicSupervisorDashboard;
---
+++ Front/my-react-app/src/pages/WorkplaceSupervisorDashboard.jsx
@@ -0,0 +1,4 @@
+function WorkplaceSupervisorDashboard() {
+return <div><h2>WorkplaceSupervisor Dashboard</h2><p>Welcome, Workplace supervisor.</p></div>;
+}
+export default WorkplaceSupervisorDashboard;
