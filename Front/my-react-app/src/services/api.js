76839e14937c99be48b4924d59a6e02ad691ca06
SSEMUWEMBA JOSEPH C <167604440+Psychecj@users.noreply.github.com>
2026-04-07 13:12:11 +0300

    Refactor loginUser to use destructured parameters

--- Front/my-react-app/src/services/api.js
+++ Front/my-react-app/src/services/api.js
@@ -29,7 +29,7 @@ async function apiFetch(path, options = {}) {
 }
 
 // now the authentiaction functions
-export async function loginUser(email, password) {
+export async function loginUser({email, password}) {
   return apiFetch('/login/',{
     method: 'POST',
     body: JSON.stringify({email, password}),
