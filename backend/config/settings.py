5345535ec165a057b58639bccb63052ddcfb4554
FAiSAL ALI <faisal.ali.doka>
2026-05-08 12:17:07 +0300

    tried sqlite3 instead of postgres but now back

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,6 +89,14 @@ DATABASES = {
         'PORT': config('DB_PORT', default='5432'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
 
 #Django REST Framework 
 REST_FRAMEWORK = {
